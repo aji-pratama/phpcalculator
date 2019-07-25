730d4ec3cf3026254d4aff3bdaa4e7a904ddbdc6
Aji Pratama <aji.p@go-jek.com>
2019-07-26 00:39:34 +0700

    Add multiply command

---
+++ src/Commands/MultiplyCommand.php
@@ -0,0 +1,34 @@
+<?php 
+
+namespace Jakmall\Recruitment\Calculator\Commands;
+
+use Jakmall\Recruitment\Calculator\Commands\AddCommand;
+
+class MultiplyCommand extends AddCommand
+{
+    protected function getCommandVerb(): string
+    {
+        return 'multiply';
+    }
+
+    protected function getCommandPassiveVerb(): string
+    {
+        return 'multiplied';
+    }
+
+    protected function getOperator(): string
+    {
+        return '*';
+    }
+
+    /**
+     * @param int|float $number1
+     * @param int|float $number2
+     *
+     * @return int|float
+     */
+    protected function calculate($number1, $number2)
+    {
+        return $number1 * $number2;
+    }
+}
